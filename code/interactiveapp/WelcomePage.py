6a9a12f56d3e521df001665c91a56a79a86606c9
loganray48 <104516932+loganray48@users.noreply.github.com>
2023-03-25 12:41:13 -0600

    ab

--- code/interactiveapp/WelcomePage.py
+++ code/interactiveapp/WelcomePage.py
@@ -7,7 +7,20 @@ import os
 # from visual_displays.demo import demonstration
 
 # Sidebar Section
-st.sidebar.header("Quantum Chemistry")
+st.sidebar.header("Zinc Molecular Weight")
+
+def welcome_page():
+    st.write("# GNN Zinc Molecular Weight Prediction")
+    st.write("Welcome to the Quantum Chemistry app. This app is designed to help you understand the basics of quantum chemistry and its applications in the real world.")
+    st.write("## What we are trying to solve")
+    st.write("Quantum chemistry is a field of study that seeks to explain and predict the behavior of molecules and atoms using quantum mechanics. This app will help you learn about the basics of quantum mechanics and how it applies to chemistry.")
+    st.write("## Application in the real world")
+    st.write("Quantum chemistry has many applications in the real world. For example, it can be used to predict the behavior of chemical reactions, design new drugs, and develop new materials with specific properties.")
+
+welcome_page()
+
+
+
 
 # button1 = st.sidebar.button("Welcome Page")
 # button2 = st.sidebar.button("What is a Graph Neural Network")
