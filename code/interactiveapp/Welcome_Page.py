deec7629b9f93cfff57b821cc1507c223c70fa59
Michael Harris <michael@Michaels-MacBook-Air.local>
2023-03-25 13:17:07 -0600

    welcome title change

--- code/interactiveapp/Welcome_Page.py
+++ code/interactiveapp/Welcome_Page.py
@@ -11,7 +11,7 @@ import os
 
 
 def welcome_page():
-    st.write("# GNNs for Drug Discovery: Analyzing Zinc Database")
+    st.write("# Graph Neural Networks for Drug Discovery")
     st.write("Today, we'll be discussing how a Graph Neural Network, or GNN, can be used to analyze the Zinc database and identify potential drug candidates. The Zinc database is a large collection of purchasable compounds that can be used for drug discovery. However, identifying potential drug candidates from this database is a challenging task, as the compounds are represented as nodes in a graph with various relationships, such as similarity or chemical properties, represented as edges in the graph.")
     st.write("## What we are trying to solve")
     st.write("Our objective is to use a Graph Neural Network (GNN) to analyze the Zinc database and identify potential drug candidates. The Zinc database is a large collection of purchasable compounds that can be used for drug discovery, but traditional methods of identifying potential drug candidates from this database are limited. By leveraging the graph structure of the database and using a GNN, we aim to overcome these limitations and discover new drugs that could save lives.")
